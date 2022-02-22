bc2800e4dd37c3ab4938aa3d683bbde7e8302da9
7Mohit <100085546+7Mohit@users.noreply.github.com>
2022-02-22 13:59:00 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Assignment 1 Python
+
+# 1- Values: hello, -87.8, 6
+#    Expressions:*, -, /, +
+#   
+#   
+# 2- String: A set of character variable. eg: "Python"
+#     vs
+#    Variable:These are symbolic representation of any value. Its type can be an integer, complex number, string etc. 
+#    eg: a="Anaconda" here a is string variable.
+#    
+#    
+# 3- Data types: Classification or categories of data items.Some types are given below:
+#     *Integer: Value is represented by int. Contains positive or negative whole numbers. Eg: int a=500
+#     *String: Value represented by str. It is a set of characters and written inside double quotes. str s= "Spyder"
+#     *Complex Numbers: Value is represented by complex. Contains real and imaginary part. Eg: complex c= 5+6j
+# 
+# 4- An expression is made up of combination of operators and operands. 
+#    Expressions are interpreted to produce some value with the help of operators and operands.
+#    
+#    
+# 5- spam=10
+#     
+#     spam=spam+1 #This is an expression.(leads to a value)
+#     print(spam) #This is a statement (Does what the statement says)
+#     
+#  
+# 
+
+# In[2]:
+
+
+#6
+bacon=22
+bacon+1
+print(bacon)
+
+
+# In[3]:
+
+
+#7
+a='spam'+ 'spamspam'
+b='spam'*3
+print(a)
+print(b)
+
+
+# 8- A variable name cannot start with a numerical value thus, egg can be variable name and 100 can't be.
+# 
+# 9-  The following functions can be used:
+#     int() for Integer
+#     float() for float
+#     str() for String
+#     
+# 
+
+# In[5]:
+
+
+#10
+#('I have eaten'+ 99 +'burritos')This statement is wrong because you can't join an integer(99) and string variables
+
+'I have eaten '+ str(99) +' burritos' #Convert the integer value to string value
+
+
+# In[ ]:
+
+
+
+
